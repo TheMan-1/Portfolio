c69c65ea00155c6a61399501ef8e61fd1c30ca43
Gage Woodall <gagewoodall0831@gmail.com>
2024-07-30 20:03:09 -0500

    a

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,27 +1,27 @@
-"use client"
+"use client";
 
-import { useEffect, useRef } from "react"
+import { useEffect, useRef } from "react";
 
-import { Content, KeyTextField } from "@prismicio/client"
-import { SliceComponentProps } from "@prismicio/react"
-import { gsap } from "gsap"
-import Bounded from "@/components/Bounded"
-import Shapes from "./Shapes"
+import { Content, KeyTextField } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+import { gsap } from "gsap";
+import Bounded from "@/components/Bounded";
+import Shapes from "./Shapes";
 
 /**
  * Props for `Hero`.
  */
-export type HeroProps = SliceComponentProps<Content.HeroSlice>
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 
 /**
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
-  const component = useRef(null)
+  const component = useRef(null);
 
   useEffect(() => {
     let ctx = gsap.context(() => {
-      const tl = gsap.timeline()
+      const tl = gsap.timeline();
 
       tl.fromTo(
         ".name-animation",
@@ -42,8 +42,8 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             each: 0.1,
             from: "random",
           },
-        }
-      )
+        },
+      );
 
       tl.fromTo(
         ".job-title",
@@ -58,14 +58,14 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
           duration: 1,
           scale: 1,
           ease: "elastic.out(1,0.3)",
-        }
-      )
-    }, component)
-    return () => ctx.revert()
-  }, [])
+        },
+      );
+    }, component);
+    return () => ctx.revert();
+  }, []);
 
   const renderLetters = (name: KeyTextField, key: string) => {
-    if (!name) return
+    if (!name) return;
     return name.split("").map((letter, index) => (
       <span
         key={index}
@@ -73,8 +73,8 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       >
         {letter}
       </span>
-    ))
-  }
+    ));
+  };
 
   return (
     <Bounded
@@ -82,7 +82,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
       ref={component}
     >
-      <div className="grid min-h-[60vh] grid-cols-1 md:grid-cols-2 items-center">
+      <div className="grid min-h-[60vh] grid-cols-1 items-center md:grid-cols-2">
         <Shapes />
         <div className="col-start-1 md:row-start-1">
           <h1
@@ -98,16 +98,13 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
               {renderLetters(slice.primary.last_name, "last")}
             </span>
           </h1>
-          <span
-            className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-300 to-yellow-500 
-          bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl"
-          >
+          <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-300 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">
             {slice.primary.tag_line}
           </span>
         </div>
       </div>
     </Bounded>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
